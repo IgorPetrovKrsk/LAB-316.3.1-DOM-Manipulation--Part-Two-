7c6c75c6421c2f1b4a56dc7299c4913044b81e1c
IgorPetrovKrsk <igor.petrov.krsk@gmail.com>
2025-03-27 15:04:56 -0600

    Part 4 done

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,33 @@
 // Menu data structure
+// var menuLinks = [
+//     { text: 'about', href: '/about' },
+//     { text: 'catalog', href: '/catalog' },
+//     { text: 'orders', href: '/orders' },
+//     { text: 'account', href: '/account' },
+//   ];
 var menuLinks = [
     { text: 'about', href: '/about' },
-    { text: 'catalog', href: '/catalog' },
-    { text: 'orders', href: '/orders' },
-    { text: 'account', href: '/account' },
-  ];
+    {
+        text: 'catalog', href: '#', subLinks: [
+            { text: 'all', href: '/catalog/all' },
+            { text: 'top selling', href: '/catalog/top' },
+            { text: 'search', href: '/catalog/search' },
+        ]
+    },
+    {
+        text: 'orders', href: '#', subLinks: [
+            { text: 'new', href: '/orders/new' },
+            { text: 'pending', href: '/orders/pending' },
+            { text: 'history', href: '/orders/history' },
+        ]
+    },
+    {
+        text: 'account', href: '#', subLinks: [
+            { text: 'profile', href: '/account/profile' },
+            { text: 'sign out', href: '/account/signout' },
+        ]
+    },
+];
 
 //part 1
 let mainEl = document.querySelector(`main`);
@@ -32,3 +55,26 @@ let subMenuEl = document.getElementById(`sub-menu`);
 subMenuEl.style.height = `100%`;
 subMenuEl.style.backgroundColor = `var(--sub-menu-bg)`;
 subMenuEl.classList.add(`flex-around`);
+
+//Part 4
+
+let topMenuLinks = topMenuEl.querySelectorAll(`a`);
+
+topMenuEl.onclick = topMenuElClickListener;
+
+function topMenuElClickListener(event) {
+    event.preventDefault();
+    if (event.target.nodeName != `A`) {
+        return;
+    } else {
+        //console.log(event.target);
+        event.target.classList.toggle(`active`);
+        for (it of topMenuLinks){ //only `a` elements here
+            if (it === event.target){
+                continue;
+            }
+            it.classList.remove(`active`);
+        }
+    }
+
+}
