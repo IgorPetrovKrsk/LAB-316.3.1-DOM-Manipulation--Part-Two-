0993935b97bbcf34a45823b749fe794e1ccb9d7d
IgorPetrovKrsk <igor.petrov.krsk@gmail.com>
2025-03-27 14:03:34 -0600

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,27 @@
+// Menu data structure
+var menuLinks = [
+    { text: 'about', href: '/about' },
+    { text: 'catalog', href: '/catalog' },
+    { text: 'orders', href: '/orders' },
+    { text: 'account', href: '/account' },
+  ];
+
+//part 1
+let mainEl = document.querySelector(`main`);
+mainEl.style.backgroundColor = `var(--main-bg)`;
+mainEl.innerHTML = `<h1>DOM Manipulation</h1>`;
+mainEl.classList.add(`flex-ctr`);
+
+//part 2
+let topMenuEl = document.getElementById(`top-menu`);
+topMenuEl.style.backgroundColor = `var(--top-menu-bg)`;
+topMenuEl.style.height = `100%`;
+topMenuEl.classList.add(`flex-around`);
+
+//part 3
+menuLinks.forEach((it) => {
+    let newA = document.createElement('a');
+    newA.href = it.href;
+    newA.textContent = it.text;
+    topMenuEl.append(newA);
+})
